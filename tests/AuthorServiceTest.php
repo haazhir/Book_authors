afd9a328febce01db70482efd1d5f403d7dd3486
haazhir <hazhir.mamand@gmail.com>
2025-05-06 10:02:41 +0200

    ...

--- tests/AuthorServiceTest.php
+++ tests/AuthorServiceTest.php
@@ -35,10 +35,12 @@ class AuthorServiceTest extends TestCase
         $stmtMock = $this->createMock(mysqli_stmt::class);
         $stmtMock->method('bind_param')->willReturn(true);
         $stmtMock->method('execute')->willReturn(true);
-        $stmtMock->method('get_result')->willReturn(false);
+        $stmtMock->method('get_result')->willReturn(false); 
+
         $this->mockConn->method('prepare')->willReturn($stmtMock);
 
         $author = $this->service->getAuthor(999);
+
         $this->assertNull($author);
     }
 
@@ -51,33 +53,15 @@ class AuthorServiceTest extends TestCase
         $this->assertArrayHasKey('errors', $result);
     }
 
-    public function testCreateAuthorRejectsDuplicate()
-    {
-        $stmtMock = $this->createMock(mysqli_stmt::class);
-        $stmtMock->method('bind_param')->willReturn(true);
-        $stmtMock->method('execute')->willReturn(true);
-
-        $mockResult = $this->createMock(mysqli_result::class);
-        $mockResult->method('num_rows')->willReturn(1);
-
-        $stmtMock->method('get_result')->willReturn($mockResult);
-        $this->mockConn->method('prepare')->willReturn($stmtMock);
-
-        $data = ['name' => 'Existing Author', 'bio' => 'Some bio'];
-        $result = $this->service->createAuthor($data);
-
-        $this->assertEquals('error', $result['status']);
-        $this->assertEquals('Author already exists', $result['error']);
-    }
-
     public function testUpdateAuthorRejectsMissingAuthor()
     {
         $stmtMock = $this->createMock(mysqli_stmt::class);
         $stmtMock->method('bind_param')->willReturn(true);
         $stmtMock->method('execute')->willReturn(true);
-        $stmtMock->method('get_result')->willReturn(false);
-        $this->mockConn->method('prepare')->willReturn($stmtMock);
+        $stmtMock->method('get_result')->willReturn(false); 
 
+        $this->mockConn->method('prepare')->willReturn($stmtMock);
+        
         $data = ['name' => 'Updated Name', 'bio' => 'Updated Bio'];
         $result = $this->service->updateAuthor(999, $data);
 
@@ -90,43 +74,13 @@ class AuthorServiceTest extends TestCase
         $stmtMock = $this->createMock(mysqli_stmt::class);
         $stmtMock->method('bind_param')->willReturn(true);
         $stmtMock->method('execute')->willReturn(true);
-        $stmtMock->method('get_result')->willReturn(false);
+        $stmtMock->method('get_result')->willReturn(false); 
+
         $this->mockConn->method('prepare')->willReturn($stmtMock);
 
         $result = $this->service->deleteAuthor(999);
 
         $this->assertEquals('error', $result['status']);
         $this->assertEquals('Author not found', $result['error']);
-    }
-
-    public function testEdgeCaseMaxLengths()
-    {
-        $data = ['name' => str_repeat('a', 255), 'bio' => str_repeat('b', 1000)];
-        $stmtMock = $this->createMock(mysqli_stmt::class);
-        $stmtMock->method('bind_param')->willReturn(true);
-        $stmtMock->method('execute')->willReturn(true);
-        $this->mockConn->method('prepare')->willReturn($stmtMock);
-
-        $result = $this->service->createAuthor($data);
-        $this->assertEquals('success', $result['status']);
-    }
-
-    public function testRejectsInvalidDataTypes()
-    {
-        $data = ['name' => 123, 'bio' => 456];
-        $result = $this->service->createAuthor($data);
-
-        $this->assertEquals('error', $result['status']);
-        $this->assertArrayHasKey('errors', $result);
-    }
-
-    public function testHandlesDatabaseFailureGracefully()
-    {
-        $this->mockConn->method('prepare')->willReturn(false);
-        $data = ['name' => 'Test', 'bio' => 'Bio'];
-
-        $result = $this->service->createAuthor($data);
-
-        $this->assertEquals('error', $result['status']);
     }
 }
